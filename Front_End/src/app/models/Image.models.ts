80ab8ee1f486ae6ea9ae50c9caa47bd5d06d51e9
BenMrad Bilel <109316958+BenMradB@users.noreply.github.com>
2023-05-18 12:59:26 +0200

    Delete Front_End/src/app directory

--- Front_End/src/app/models/Image.models.ts
+++
@@ -1,6 +0,0 @@
-export interface Image {
-  imageId: number;
-  imageName: string;
-  imageType: string;
-  image: number[]
-}
--- Front_End/src/app/models/Speciality.model.ts
+++
@@ -1,4 +0,0 @@
-export interface Speciality {
-  specialityId: number;
-  specialityName: string;
-}
